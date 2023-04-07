9069f9967c76bf03d74c47db19b027eebb0d19dc
Yang Enmanuel <enmanuelcalzat@gmail.com>
2023-04-07 12:55:13 -0300

    Added flattening exercise

---
+++ Part1/Chapter 5/flattening.js
@@ -0,0 +1,14 @@
+/*
+  Flattening
+Use the reduce method in combination with the concat method to “flatten” an array 
+of arrays into a single array that has all the elements of the original arrays.
+*/
+
+function flatter (arr) {
+  return arr.reduce((acc, curr) => {
+    return acc.concat(curr)
+  }, [])
+}
+
+console.log(flatter([[1, 2, 3], [4, 5], [6]]))
+// => [1,2,3,4,5,6]
